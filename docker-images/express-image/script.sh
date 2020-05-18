9c615d9fee137e18503abecc5614043bb7e3c0c6
fabano237 <fabrice.mbassinguema@heig-vd.ch>
2020-05-18 11:33:41 +0200

    add de script.sh for this test

---
+++ docker-images/express-image/script.sh
@@ -0,0 +1,13 @@
+#!/bin/bash
+
+# Entré user nom image 
+read -p "entrré le nom de l'image: " IMAGE
+
+# Vérifications des entrées utilisateur
+if [ -z "$IMAGE" ]; then
+    exit
+fi
+docker kill $(docker ps -q --filter ancestor=$IMAGE)
+docker rm   $(docker ps -a -q --filter ancestor=$IMAGE)
+docker build -t $IMAGE .
+docker run $IMAGE
--- docker-images/express-image/src/index.js
+++ docker-images/express-image/src/index.js
@@ -2,4 +2,4 @@ var Chance = require('chance');
 
 var chance = new Chance();
 
-console.log("Bonjour" + chance.name());
+console.log("Bonjour " + chance.name());
