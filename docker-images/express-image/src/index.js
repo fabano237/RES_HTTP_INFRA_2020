6c9fb25f466be22527849b8e8dce5ae58d2f6850
fabano237 <fabrice.mbassinguema@heig-vd.ch>
2020-05-18 14:41:27 +0200

    Added a Docker image wrapping an express.js that returns random arrays of students

--- docker-images/express-image/src/index.js
+++ docker-images/express-image/src/index.js
@@ -2,4 +2,28 @@ var Chance = require('chance');
 
 var chance = new Chance();
 
-console.log("Bonjour " + chance.name());
+const express = require('express');
+const app = express();
+const port = 2020;
+
+app.get('/', (req, res) => res.send(generateStudents()));
+app.listen(port,() => console.log(`Accepting HTTP request on: ${port}`));
+
+function generateStudents(){
+    var numberOfstudents = chance.integer({min:0, max: 10});
+    console.log(numberOfstudents);
+    var students = [];
+
+    for(var i = 0; i < numberOfstudents; ++i){
+        var gender = chance.gender();
+        var birthYear = chance.year({min: 1993, max: 2020});
+
+        students.push({firstName: chance.first({gender: gender}),
+                       lastName:  chance.last(),
+                       gender:    gender,
+                       birthday:  chance.birthday({year: birthYear})});
+    };
+
+    console.log(students + '\n');
+    return students;
+}
